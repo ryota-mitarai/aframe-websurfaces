7b3277dec69bdfe87bd800031b993235c63bf25f
ryota-mitarai <ryotamitarai0@gmail.com>
2021-05-21 16:09:20 -0400

    style: formatting

--- src/websurface.js
+++ src/websurface.js
@@ -10,37 +10,46 @@ export const component = AFRAME.registerComponent('websurface', {
     frameSkips: { default: 1 },
     autoSceneStyling: { default: true },
   },
+
   init: function () {
     const el = this.el;
     const data = this.data;
+
     if (data.autoSceneStyling == true) {
       el.sceneEl.style.position = 'absolute';
       el.sceneEl.style.zIndex = '2';
     }
+
     if (data.isInteractable == true) {
       data.mouseHasLeftScreen = true;
       //geometry for click detection
       el.setAttribute('geometry', `primitive:plane; width:${data.width}; height:${data.height};`);
+
       el.addEventListener('click', function () {
         if (data.mouseHasLeftScreen == false) return;
         document.exitPointerLock();
         el.sceneEl.style.zIndex = -2;
         data.mouseHasLeftScreen = false;
       });
+
       el.addEventListener('mouseleave', function () {
         data.mouseHasLeftScreen = true;
       });
     }
+
     el.addEventListener('cam-loaded', function () {
       const iframe = document.createElement('iframe');
       iframe.setAttribute('src', data.url);
       iframe.style.border = 'none';
+
       const camera = el.sceneEl.camera;
       const context = new DOMContext(camera, el);
       context.setSize(window.innerWidth, window.innerHeight);
       document.body.appendChild(context.domElement);
+
       const element = new DOMElement(context, iframe, data.width, data.height);
       el.object3D.add(element);
+
       if (data.isInteractable == true) {
         const div = document.createElement('div');
         div.style.position = 'fixed';
@@ -53,9 +62,11 @@ export const component = AFRAME.registerComponent('websurface', {
           el.sceneEl.style.zIndex = 2;
         });
       }
+
       this.websurface_iframe = iframe;
       data.context = context;
       data.element = element;
+
       window.addEventListener('resize', () => {
         context.setSize(window.innerWidth, window.innerHeight);
       });
@@ -63,6 +74,7 @@ export const component = AFRAME.registerComponent('websurface', {
     data.frames = 0;
     data.isCamLoaded = false;
   },
+
   tick: function () {
     const el = this.el;
     const data = this.data;
@@ -77,6 +89,7 @@ export const component = AFRAME.registerComponent('websurface', {
       }
       return;
     }
+
     const context = data.context;
     const element = data.element;
     if (data.frames % data.frameSkips == 0) {
@@ -87,8 +100,10 @@ export const component = AFRAME.registerComponent('websurface', {
         element.update(el.object3D);
       }
     }
+
     data.frames++;
   },
+
   pause: function () {
     const data = this.data;
 
